5714ace1f0a383ec3945b1ac194887a544d51e03
Aamir Mirza <spookane112@gmail.com>
2021-11-03 15:39:33 -0400

    Finished up to Question 16

--- ch7/reinforcement/r.py
+++ ch7/reinforcement/r.py
@@ -418,6 +418,12 @@ class PositionalList(_DoublyLinkedBase):
             yield cursor.element()
             cursor = self.after(cursor)
 
+    def __reversed__(self):
+        cursor = self.last()
+        while cursor is not None:
+            yield cursor.element()
+            cursor = self.before(cursor)
+
     # ------------------------------- mutators -------------------------------
     # override inherited version to return Position, rather than Node
     def _insert_between(self, e, predecessor, successor):
@@ -553,3 +559,170 @@ def test_recursive_find(to_find, *numbers):
 # R-7.15
 """Provide support for a __reversed__ method of the PositionalList class that is similar to the given __iter__, but that
 iterates the elements in reversed order."""
+
+def test_reverse(*numbers):
+    """
+    >>> test_reverse(10, 6, 2, 4, 5, 52)
+    [52, 5, 4, 2, 6, 10]
+    """
+    L = PositionalList()
+    for number in numbers:
+        L.add_first(number)
+    return [reverse_number for reverse_number in L]
+
+
+# R-7.16
+"""Describe an implementation of the PositionalList methods add_last and add_before realized by using only methods in 
+the set {is_empty, first, last, prev, next, add_after and add_first}."""
+
+
+class PositionalList2(_DoublyLinkedBase):
+    """A sequential container of elements allowing positional access."""
+
+    # -------------------------- nested Position class --------------------------
+    class Position:
+        """An abstraction representing the location of a single element.
+
+        Note that two position instaces may represent the same inherent
+        location in the list.  Therefore, users should always rely on
+        syntax 'p == q' rather than 'p is q' when testing equivalence of
+        positions.
+        """
+
+        def __init__(self, container, node):
+            """Constructor should not be invoked by user."""
+            self._container = container
+            self._node = node
+
+        def element(self):
+            """Return the element stored at this Position."""
+            return self._node._element
+
+        def __ge__(self, other):
+            if type(other.element()) is type(self.element()):
+                return self.element() >= other.element()
+            else:
+                raise TypeError
+
+        def __gt__(self, other):
+            if type(other.element()) is type(self.element()):
+                return self.element() > other.element()
+            else:
+                raise TypeError
+
+        def __lt__(self, other):
+            if type(other.element()) is type(self.element()):
+                return self.element() < other.element()
+            else:
+                raise TypeError
+
+        def __le__(self, other):
+            if type(other.element()) is type(self.element()):
+                return self.element() <= other.element()
+            else:
+                raise TypeError
+
+        def __eq__(self, other):
+            """Return True if other is a Position representing the same location."""
+            if type(other) is type(self):
+                if type(other.element()) is type(self.element()):
+                    return other.element() == self.element()
+            return False
+
+        def __ne__(self, other):
+            """Return True if other does not represent the same location."""
+            return not (self == other)  # opposite of __eq__
+
+    # ------------------------------- utility methods -------------------------------
+    def _validate(self, p):
+        """Return position's node, or raise appropriate error if invalid."""
+        if not isinstance(p, self.Position):
+            raise TypeError('p must be proper Position type')
+        if p._container is not self:
+            raise ValueError('p does not belong to this container')
+        if p._node._next is None:  # convention for deprecated nodes
+            raise ValueError('p is no longer valid')
+        return p._node
+
+    def _make_position(self, node):
+        """Return Position instance for given node (or None if sentinel)."""
+        if node is self._header or node is self._trailer:
+            return None  # boundary violation
+        else:
+            return self.Position(self, node)  # legitimate position
+
+    # ------------------------------- accessors -------------------------------
+    def first(self):
+        """Return the first Position in the list (or None if list is empty)."""
+        return self._make_position(self._header._next)
+
+    def last(self):
+        """Return the last Position in the list (or None if list is empty)."""
+        return self._make_position(self._trailer._prev)
+
+    def before(self, p):
+        """Return the Position just before Position p (or None if p is first)."""
+        node = self._validate(p)
+        return self._make_position(node._prev)
+
+    def after(self, p):
+        """Return the Position just after Position p (or None if p is last)."""
+        node = self._validate(p)
+        return self._make_position(node._next)
+
+    def __iter__(self):
+        """Generate a forward iteration of the elements of the list."""
+        cursor = self.first()
+        while cursor is not None:
+            yield cursor.element()
+            cursor = self.after(cursor)
+
+    def __reversed__(self):
+        cursor = self.last()
+        while cursor is not None:
+            yield cursor.element()
+            cursor = self.before(cursor)
+
+    # ------------------------------- mutators -------------------------------
+    # override inherited version to return Position, rather than Node
+    def _insert_between(self, e, predecessor, successor):
+        """Add element between existing nodes and return new Position."""
+        node = super()._insert_between(e, predecessor, successor)
+        return self._make_position(node)
+
+    def add_first(self, e):
+        """Insert element e at the front of the list and return new Position."""
+        return self._insert_between(e, self._header, self._header._next)
+
+    def add_last(self, e):
+        """Insert element e at the back of the list and return new Position."""
+        # return self._insert_between(e, self._trailer._prev, self._trailer)
+        if self.is_empty():
+            self.add_first(e)
+        else:
+            self.add_after(self.last(), e)
+
+    def add_before(self, p, e):
+        """Insert element e into list before Position p and return new Position."""
+        original = self._validate(p)
+        return self._insert_between(e, original._prev, original)
+
+    def add_after(self, p, e):
+        """Insert element e into list after Position p and return new Position."""
+        original = self._validate(p)
+        return self._insert_between(e, original, original._next)
+
+    def delete(self, p):
+        """Remove and return the element at Position p."""
+        original = self._validate(p)
+        return self._delete_node(original)  # inherited method returns element
+
+    def replace(self, p, e):
+        """Replace the element at Position p with e.
+
+        Return the element formerly at Position p.
+        """
+        original = self._validate(p)
+        old_value = original._element  # temporarily store old element
+        original._element = e  # replace with new element
+        return old_value  # return the old element value
